da295218c8a8c0f6d94cd56adc5537285a1eda55
VonDyring <ch.alfredsson@gmail.com>
2019-06-24 20:10:39 +0200

    initial commit

---
+++ rainbow_toilet_m4.ino
@@ -0,0 +1,53 @@
+#include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
+
+
+/******************************************************************************
+ * Defines
+ */
+
+#define NR_PIXELS (8u)
+
+/******************************************************************************
+ * Globals
+ */
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NR_PIXELS, 2);
+Adafruit_NeoPixel onb = Adafruit_NeoPixel(1,40);
+
+uint32_t rgbc = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+
+  strip.begin();
+  strip.clear();
+  onb.begin();
+  onb.clear();
+
+}
+
+
+void loop()
+{
+  delay(1000);
+  Serial.println("Red!");
+  rgbc = strip.gamma32(strip.ColorHSV(0, 250, 100));
+  strip.fill(rgbc,0,0);
+  onb.fill(rgbc,0,0);
+  Serial.print(rgbc);Serial.print("\t");Serial.println(strip.getBrightness());
+
+  strip.show();
+  onb.show();
+  delay(1000);
+
+  Serial.println("Bluish!");
+  rgbc = strip.gamma32(strip.ColorHSV(30000, 250, 100));
+  strip.fill(rgbc, 0,0);
+  onb.fill(rgbc, 0,0);
+  Serial.print(rgbc);Serial.print("\t");Serial.println(onb.getBrightness());
+  strip.show();
+  onb.show();
+}
---
+++ spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ C:/Users/Christian/dev/sloeber-workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
